691f7145d1754995f93c0e1468d45a14536db3ce
fdlessard <fdlessard@gmail.com>
2020-05-12 16:52:10 -0400

    jenkins push

--- src/main/java/io/fdlessard/codebites/msgraph/user/GroupController.java
+++ src/main/java/io/fdlessard/codebites/msgraph/user/GroupController.java
@@ -21,6 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class GroupController {
 
   @Autowired
+
   private OAuth2AuthorizedClientService oAuth2AuthorizedClientService;
 
   @GetMapping("/group")
